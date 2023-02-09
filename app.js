618395881afe830b38483516314c5b375abdeb5f
vishaluniv <vishal4855.be21@chitkara.edu.in>
2023-02-09 19:32:06 +0530

    Update

--- app.js
+++ app.js
@@ -19,18 +19,22 @@ devices.forEach(function (device) {
     table.appendChild(row);
 });
 
-devices.forEach(function(device) {
-  $('#devices tbody').append(`
-    <tr>
-      <td>${device.user}</td>
-      <td>${device.name}</td>
-    </tr>`
-  );
+
+
+devices.forEach(function (device) {
+    $('#devices tbody').append(`
+      <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+      </tr>`
+    );
 });
 
-document.querySelector('#add-device').addEventListener('click', function() {
-  const user = document.querySelector('#user').value;
-  const name = document.querySelector('#name').value;
-  devices.push({ user: user, name: name });
-  console.log(devices);
+
+$('#add-device').on('click', function () {
+    const user = $('#user').val();
+    const name = $('#name').val();
+    devices.push({ user: user, name: name });
+    localStorage.setItem('devices', JSON.stringify(devices));
+    location.href="devices-list.html";
 });
