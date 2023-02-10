22f6624e0dbfd76753a7cc46ec99605b0809a997
vishaluniv <vishal4855.be21@chitkara.edu.in>
2023-02-10 09:33:17 +0530

    App updated

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
 
-devices.forEach(function (device) {
-    const table = document.querySelector('#devices');
-    const row = document.createElement('tr');
-
-    const user = document.createElement('td');
-    const userText = document.createTextNode(device.user);
-    user.appendChild(userText);
-
-    const name = document.createElement('td');
-    const nameText = document.createTextNode(device.name);
-    name.appendChild(nameText);
-
-    row.appendChild(user);
-    row.appendChild(name);
-
-    table.appendChild(row);
-});
+devices.forEach(function(device) {
+    $('#devices tbody').append(`
+      <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+      </tr>`
+    );
+  });
 
 
 
